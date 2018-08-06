7d82aded24fd317f5b1b65e6b3cc4a882c445481
usrrname <usrrname@users.noreply.github.com>
2018-08-06 16:12:44 -0400

    cleaning up the funcs to return whole sentences esp refine()

--- public/app/services/filter.srv.js
+++ public/app/services/filter.srv.js
@@ -11,6 +11,7 @@ function filterSrv($http) {
     ctrl.popObj={};
     ctrl.popTweets=[];
     ctrl.popDate=[];
+    ctrl.finalTweets = [];
 
     //functions
     ctrl.getTweets = getTweets;
@@ -26,10 +27,10 @@ function filterSrv($http) {
                         ctrl.tweets.push(item.tweet)
                         ctrl.recentDate.push(item.timeElapsed);
                     })
-                  var helpfulElf = refine(ctrl.tweets);
-                  helpfulElf.forEach(function(item, i){
-                    ctrl.refinedTweets.push(helpfulElf[i]);
-                  })
+                    for (var i = 0; i < ctrl.tweets.length; i++) {
+                        var z = refine(ctrl.tweets[i]);
+                        ctrl.refinedTweets.push(z[i]);
+                    }
                 }
                 else {
                     res.json(err);
@@ -42,31 +43,29 @@ function popularTweets() {
             .then(function (res) {
                 ctrl.popObj = res.data;
                 if (res.status === 200) {
-                    var tempTweets=[];
-                    ctrl.popObj.forEach(function (item, index) {
-                        tempTweets.push(item.tweet);
+                    ctrl.popObj.forEach(function (item, i) {
+                        ctrl.popTweets.push(item.tweet);
                         ctrl.popDate.push(item.timeElapsed);
                     });
-                    var helper = refine(ctrl.tempTweets);
-                    helper.forEach(function (item, i) {
-                        ctrl.popTweets.push(helper[i]);
-                        console.log(ctrl.popTweets);
-                    });
-                    return ctrl.popObj;
+                    for (var i = 0; i < ctrl.popTweets.length; i++) {
+                       var refined = refine(ctrl.popTweets[i]);
+                       ctrl.finalTweets.push(refined[i]);
+                   }
             } 
                 else {
                     res.json(err);
                 }
-            })
+                return ctrl.popObj;
+            });
     } 
 // end of popularTweets()
     function refine(incomingArray) {
-
-           var wholeTweets = [];
+           var wholeTweets=[];
+           incomingArray.split(",");
            for (var i = 0; i < incomingArray.length; i++) {
-               pTweets = []; 
-                //turn html entities into symbols
-               var str = decodeURI(incomingArray[i]);
+                var pTweets = []; 
+                //turn html entities into symbols or text
+                var str = unescape(incomingArray);
                 //remove hashtags
                 var removeHash = new RegExp('#([^\\s]*)', '');
                 str.replace(removeHash, '');
@@ -76,16 +75,19 @@ function popularTweets() {
                 //remove emojis
                 var noEmojis = str.replace('/([\uE000-\uF8FF]|\uD83C[\uDF00-\uDFFF]|\uD83D[\uDC00-\uDDFF])/g', '');
                 pTweets.push(str);
+                //remove urls
                 for (var a = 0; a < pTweets.length; a++) { 
-                    // remove urls
                     var httpLoc = pTweets[a].indexOf('https://t.co/');
                     if (httpLoc != -1 || httpLoc == ' ') {
                         var beforeHttp = pTweets[a].substring(0, httpLoc);
                         var remaining = pTweets[a].substring(httpLoc, pTweets[a].length);
                         var noLinks = str.replace(remaining, '');
                         wholeTweets.push(noLinks);
+                    }
+                    if (httpLoc == -1){
+                        wholeTweets.push(pTweets[a]);
                     } 
-               } 
+               }
         }
         return wholeTweets;
       } 
